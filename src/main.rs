b5b787f7f041cd4fb1d7fd1cec3b527bf867f5a6
Valerian Saliou <valerian@valeriansaliou.name>
2018-01-09 12:47:06 +0100

    v0.0.1
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,9 @@
 // Copyright: 2018, Valerian Saliou <valerian@valeriansaliou.name>
 // License: Mozilla Public License v2.0 (MPL v2.0)
 
-#[macro_use]
-extern crate log;
+// #[macro_use]
+// extern crate log;
+
+fn main() {
+    // TODO
+}
